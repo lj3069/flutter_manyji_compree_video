69191d8b9d3d2a65ce1ff84fcd9de85e94504a98
LJ <975192355@qq.com>
2019-02-22 16:17:14 +0800

    example

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_manyji_deviceinfo/FlutterManyjiDeviceinfoPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterManyjiDeviceinfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterManyjiDeviceinfoPlugin"]];
+}
+
+@end
